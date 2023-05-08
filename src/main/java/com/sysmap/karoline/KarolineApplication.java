85e9da2c0ffa56dc44e170422322213a12fcb7db
Karolinegaia <drakarolinegaia@gmail.com>
2023-05-08 13:33:46 -0300

    First commit

---
+++ src/main/java/com/sysmap/karoline/KarolineApplication.java
@@ -0,0 +1,13 @@
+package com.sysmap.karoline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarolineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarolineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysmap/karoline/KarolineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmap.karoline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarolineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
